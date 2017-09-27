d4cf5a3eae7a5a86fa90d3aad9cb5944f0760723
Shahmali Hasanov <shahmalimh@code.edu.az>
2017-09-27 18:00:41 +0400

    meyyen seyler hazirdir

--- js/script.js
+++ js/script.js
@@ -2,11 +2,37 @@ var innerContent=document.querySelector(".inner-content");
 var contentItem=document.querySelectorAll(".content-item");
 var contentitemArr=[];
 var contentTopOlcu=0;
+var navLi=document.querySelectorAll(".nav ul li");
+var navLiArr=[]
+
 for (var i = 0; i < contentItem.length; i++) {
 	contentitemArr.push(contentItem[i])
+	navLiArr.push(navLi[i]);
 }
 function qaldirMans () {
- contentTopOlcu-=280;
- innerContent.style.top = contentTopOlcu+"px"
+	 
+
+contentTopOlcu-=280;
+ if (contentTopOlcu<=-2520) {
+ 	contentTopOlcu=-280*(contentitemArr.length-1)
+ }
+
+ innerContent.style.top = contentTopOlcu+"px";
+
+}
+function dusMans(){
+	say=0
+	contentTopOlcu+=280;
+	if (contentTopOlcu>=0) {
+		contentTopOlcu=0
+	}
+	else{
+		innerContent.style.top = contentTopOlcu+"px";
+	}
+	innerContent.style.top = contentTopOlcu+"px";
+}
+function selectedLi(par){
+	contentTopOlcu=-(navLiArr.indexOf(par))*280
+	innerContent.style.top = contentTopOlcu+"px";
 
 }
