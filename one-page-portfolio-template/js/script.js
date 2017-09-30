09e0e7f50188552d832f50044f2a71a61307841b
Şahmalı Həsənov <sahmali.hesenov@gmail.com>
2017-09-30 19:47:27 +0400

    aaa

--- one-page-portfolio-template/js/script.js
+++ one-page-portfolio-template/js/script.js
@@ -1,6 +1,21 @@
-$(".col-6").on("click",function (event) {
+$(".item").on("click",function () {
 	
-	$(this).show("1")
-	var hidden=$(this).find('.hidden-item')
-	$(hidden).show().css('opacity', '1').height(100+"vh");
+	
+	var hidden=$(this).parent().find(".hidden-item")
+
+	$(hidden).removeClass("hide-hidden").addClass("show-hidden")
+	$(this).parent().find(".fa-times-circle").show(1000)
+$(".fa-times-circle").on("click", function () {
+		$(".col-6").show()
+		$(".show-hidden").removeClass("show-hidden").addClass("hide-hidden");
+		$(this).parent().find(".fa-times-circle").css("display", "none");
+	})
+	
+
+	})
+$("ul li img").on("click",function(){
+	$(".slider .slider-item").each()
+	console.log((".slider .slider-item").each())
+	console.log($(this).parent().index())
 })
+
